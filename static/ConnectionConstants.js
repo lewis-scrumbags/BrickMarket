75d8841446e09f42d6f1e60631d0e37a2bd96000
Romaana Pasha <romaanazpasha@lewisu.edu>
2022-12-02 23:05:53 -0600

    updated connection.js

--- static/ConnectionConstants.js
+++ static/ConnectionConstants.js
@@ -2,7 +2,7 @@
 //MongoDB username cluster : BrickMarketUser
 //MongoDB cluster password: BrickMarketPassword
 const uri = "mongodb+srv://BrickMarketUser:BrickMarketPassword@cluster0.feqkxqj.mongodb.net/?retryWrites=true&w=majority"	
-const databaseName = "BrickMarket"
-const collectionName = "leggos"
+const databaseName = "BrickMarketDatabase"
+const collectionName = "LeggoCollection"
 
 module.exports = { uri, databaseName, collectionName };
