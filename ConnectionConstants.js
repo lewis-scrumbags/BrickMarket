153e18a8a5ebe9da1cf4905a605798169ee56bbb
Romaana Pasha <romaanazpasha@lewisu.edu>
2022-12-02 23:24:39 -0600

    Create lego

--- ConnectionConstants.js
+++
@@ -1,8 +0,0 @@
-const uri = "[[YOUR-CONNECTION-STRING-HERE]]"	
-// Example uri:
-// const uri = "mongodb+srv://testMongoDBUserName:8L4kkR8KszHZTI7S@cluster0.fei8p8f.mongodb.net/?retryWrites=true&w=majority"	
-
-const databaseName = "movieDatabase"
-const collectionName = "movies"
-
-module.exports = { uri, databaseName, collectionName };
